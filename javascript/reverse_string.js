b3c7b8f115c8e4aea9351773964b365c1deb46de
Keith Ma <keithchezzy@gmail.com>
2022-12-26 22:22:38 -0800

    dsa

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,12 @@
 function reverseString(str) {
   // type your code here
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str.charAt(i);
+  }
+  return reversed;
 }
+reverseString ('hello');
 
 if (require.main === module) {
   // add your own tests in here
